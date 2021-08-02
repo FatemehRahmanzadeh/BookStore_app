560d896fd62004d97a7e1a265385bff951e2d855
Fatemeh <rahmanzadeh8922@yahoo.com>
2021-08-03 02:30:25 +0430

    User registration and static files, Ch5, Ch6 are added

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,10 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-
 # برای را کردن تست ها: python manage.py test
 # وقتی این کامند اجرا می شود، یک دیتابیس موقت به نام دیفالت ایجاد می شود که
 # داده های تست در دیتابیس اصلی وارد نشود. بعد از اتمام موفق تست این دیتابیس پاک می شود
+from django.urls import reverse, resolve
+
+from accounts.forms import CustomUserCreationForm
+from accounts.views import SignupPageView
+
 
 class CustomUserTests(TestCase):
     def test_create_user(self):
@@ -43,3 +47,28 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_superuser)
 
 
+...
+
+
+class SignupPageTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
+
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_signup_view(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(
+            view.func.__name__,
+            SignupPageView.as_view().__name__
+        )
--- bookstore/settings.py
+++ bookstore/settings.py
@@ -35,11 +35,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party apps
+    'crispy_forms',
     # my apps:
     'accounts',
     'pages',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -121,6 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
